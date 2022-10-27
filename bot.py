defcf17f333bd0173593237663cc15a173f024d2
DevOps-Dani <wadfwa@gmail.com>
2022-10-27 15:44:32 +0100

    Typos -_-

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GIULD')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client(intents=discord.Intents.default())
 
